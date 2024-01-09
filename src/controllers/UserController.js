b5a70f81174028f50ca83931d03e8a3eb470a418
Miguel Remédios <miguelremediios@gmail.com>
2024-01-09 18:23:40 +0000

    User CRUD operations in controller model and routes

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,79 @@
 import User from "../models/User";
 
 class UserController {
+  // index
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // store
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
       res.json(newUser);
     } catch (error) {
       res.status(400).json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  // show
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({ error: ["ID is required"] });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({ error: ["User not found"] });
+      }
+      const newUserData = await user.update(req.body);
+      return res.json(newUserData);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({ error: ["ID is required"] });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({ error: ["User not found"] });
+      }
+      await user.destroy();
+      return res.json(user);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ errors: error.errors.map((err) => err.message) });
     }
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,11 @@ import userController from "../controllers/UserController";
 
 const router = Router();
 
+router.get("/", userController.index);
 router.post("/", userController.store);
+router.get("/:id", userController.show);
+router.put("/:id", userController.update);
+router.delete("/:id", userController.delete);
 
 export default router;
 
@@ -11,7 +15,7 @@ export default router;
 Controllers should contain around 5 methods
   index -> list all users -> GET
   store/create -> create a new user -> POST
-  delete -> delete a user -> DELETE
   show -> show a single user -> GET
   update -> update a user -> PUT OR PATCH
+  delete -> delete a user -> DELETE
 */
